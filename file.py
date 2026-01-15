eb8f975f13bdf5a45ef72ed1356cbe76eed176a3
clairey333 <claire.marina161@gmail.com>
2026-01-15 22:12:53 +0000

    task 1

--- file.py
+++ file.py
@@ -1,4 +1,13 @@
-tasks = []
+print("welcome to the to do list app!")
+print("your to do list:")
+tasks = ["play tennis, cook food, study for midterms, code"]
 def listdisplay():
     print(tasks)
-
+print("1: add task\n2: display tasks\n3: exit")
+options = input("\nchoose an option:")
+while options == "1" or options == "2":
+    if options == "1":
+        newtask = input("add a task:")
+        tasks.append(newtask)
+        print(tasks)
+    options = input("choose an option:")
